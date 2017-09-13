d7593bdaf1d9cdfc9270bb17f07e75d5fa6b0c81
h0we228 <31608960+h0we228@users.noreply.github.com>
2017-09-13 00:09:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,66 @@
-function openCity(evt, cityName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
+var tabLinks= new Array();
+var contentDivs = new Array();
 
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent =
-    document.getElementsByClassName("tabcontent");
-      for (i=0; i<tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-      }
+<body onload="init()">
 
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks =
-    document.getElementsByClassName("tablinks");
-      for (i=0; i<tablinks.length; i++) {
-        tablinks[i].className = tablinks [i].className.replace (" active", "");
-      }
+function init() {
+  // Grab the tab links and content divs from the page
+  var tabListItems = document.getElementById('tabs').childNodes;
+  for ( var i = 0; i < tabListItems.length; i++ ) {
+    if ( tabListItems[i].nodeName == "LI") {
+      var tabLink = getFirstChildWithTagName( tabListItems[i], 'A');
+      var id = getHash( tabLink.getAttribute('href') );
+      tabLinks[id] = tabLink;
+      contentDivs[id] = document.getElementById( id );
+    }
+  }
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+
+  // Assign onclick events to the tab links, and highlight the first tab
+  var i = 0;
+
+  for ( var id in tabLinks ) {
+    tabLinks[id].onclick = showTab;
+    tabLinks[id].onfocus = function() { this.blur()};
+    if ( i == 0 ) tabLinks[id].className = 'selected';
+    i++;
+  }
+
+  //Hide all content divs except the first
+  var i = 0;
+
+  for ( var id in contentDivs ) {
+    if ( i != 0 ) contentDivs[id].className = 'tabContent hide';
+    i++;
+  }
+}
+
+function showTab() {
+  var selectedId = getHash( this.getAttribute('href') );
+
+  // Highlight the selected tab, and dim all others.
+  // Also show the selected content div, and hide all others.
+  for ( var id in contentDivs ) {
+    if ( id == selectedId ) {
+      tabLinks[id].className = 'selected';
+      contentDivs[id].className = 'tabContent';
+    } else {
+      tabLinks[id].className = '';
+      contentDivs[id].className = 'tabContent hide';
+    }
+  }
+
+  // Stop the browser following the link
+  return false;
+}
+
+function getFirstChildWithTagName( element, tagName ) {
+  for ( var i = 0; i < element.childNodes.length; i++ ) {
+    if ( element.childNodes[i].nodeName == tagName ) return element.childNodes[i];
+  }
+}
+
+function getHash( url ) {
+  var hashPos = url.lastIndexOf ( '#' );
+  return url.substring( hashPos + 1);
 }
