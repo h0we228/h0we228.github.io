3608947e02eb8b0ac95a0273d6305ccd20b8cc09
h0we228 <31608960+h0we228@users.noreply.github.com>
2018-05-30 00:55:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 var tabLinks= new Array();
 var contentDivs = new Array();
+var accordions=document.querySelectorAll("button.accordion");
+
+for(var i=0; i<accordions.length;i++){
+  accordions[i].onclick=function(){
+    this.classList.toggle("active");
+    this.nextElementSibling.classList.toggle("show");
+  }
+}
 
 <body onload="init()">
 
