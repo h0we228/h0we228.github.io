f26546ed8e2029bb045d9a8c9d2653fed4ae7749
h0we228 <31608960+h0we228@users.noreply.github.com>
2018-05-29 20:03:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,20 @@ function init() {
     }
   }
 
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
 
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
   // Assign onclick events to the tab links, and highlight the first tab
   var i = 0;
 
